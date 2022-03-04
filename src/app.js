98f157024726ef56a0cc5e88bf3f669df88bf423
Onur ÜRE <onurure@192.168.1.50>
2022-03-04 16:53:51 +0300

    fixed

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ mongoose.connect(process.env.MONGO, {useNewUrlParser:true}).then(() => console.l
 
 app.use('/.netlify/functions/api', postsRoute)
 
+module.exports = app;
 module.exports.handler = serverless(app)
 // app.listen(3000, () => {
 //   console.log('listen')
